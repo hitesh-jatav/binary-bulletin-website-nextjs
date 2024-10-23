8ec8b4069c31348160dc64018f2dfe3d6041b903
hitesh-jatav <hkjatav@gmail.com>
2024-10-23 23:49:47 +0530

    sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    siteUrl: process.env.NEXT_PUBLIC_BLOG_URL,
+    generateRobotsTxt: true,
+    outDir: "./public",
+    generateIndexSitemap: false,
+    changefreq: "weekly",
+    priority: 0.7,
+
+};
